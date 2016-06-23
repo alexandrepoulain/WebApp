236794778d08621943b8ed4719e9f8d09ab5a1d1
alexandrepoulain <alexandrep2@hotmail.fr>
2016-06-23 13:47:59 +0200

    Process Block done

--- NCVT_JsonGenerator/client/assets/js/app.js
+++ NCVT_JsonGenerator/client/assets/js/app.js
@@ -11,8 +11,7 @@
   ])
     // service to share the id 
     .service('sharedId', function () {
-        var property = 'First';
-
+        var property = "";
         return {
             getProperty: function () {
                 return property;
@@ -23,31 +22,58 @@
         };
     })
 
-
     // control settings ProcessingBlock
     .controller('CtrlSettingProcessBlock', function($scope, sharedId) {
+
         var id = sharedId.getProperty();
-        $scope.master = {Name: "ProcessingBlock", ID: id};
+        $scope.master = {Name: "ProcessingBlock", ID: id, nameScript: "", interpreter: "", selection: "", inputs: [], outputs: [], parameters: []};
+        // add inputs
+        $scope.addInputs = function (key,value) {
+          var temp = {key,value};
+          $scope.sequence.inputs.push(temp);
+          $scope.saveProcessBlock();
+        };
+        // add outputs
+        $scope.addOutputs = function (key,value) {
+          var temp = {key,value};
+          $scope.sequence.outputs.push(temp);
+          $scope.saveProcessBlock();
+        };
+        // add element in list parameters
+        $scope.addParameter = function (key,value) {
+          var temp = {key,value};
+          $scope.sequence.parameters.push(temp);
+          $scope.saveProcessBlock();
+        };
+        // reset block
         $scope.reset = function() {
             $scope.sequence = angular.copy($scope.master);
         };
         $scope.reset();
-
+        // store the block in local
         $scope.saveProcessBlock = function () {
           
           localStorage.setItem($scope.sequence.ID, JSON.stringify($scope.sequence));
         };
-
+        // get the block by id
         $scope.uploadData = function (id) {
           // Ici, on reconvertit la chaîne en un objet 
           // JSON.stringify and saved in localStorage in JSON object again
           var sessionRestaurée = JSON.parse(localStorage.getItem(id));
-          $scope.sequence = sessionRestaurée;
+          try {
+            if (sessionRestaurée.ID != null)
+              $scope.sequence = sessionRestaurée;
+          } catch(e) {
+            // statements
+            console.log("Pas d'id pour session");
+          }
+          
           // La variable sessionRestaurée contient désormais l'objet précédent
           // qui avait été sauvegardé dans localStorage
           console.log(sessionRestaurée);
         };
     })
+
     // control settings ProcessingSequence
     .controller('CtrlSettingProcessSeq',function($scope, $http, sharedId) {
         var id = sharedId.getProperty();
@@ -66,7 +92,14 @@
           // Ici, on reconvertit la chaîne en un objet 
           // JSON.stringify and saved in localStorage in JSON object again
           var sessionRestaurée = JSON.parse(localStorage.getItem(id));
-          $scope.sequence = sessionRestaurée;
+           try {
+            if (sessionRestaurée.ID != null)
+              $scope.sequence = sessionRestaurée;
+          } catch(e) {
+            // statements
+            console.log("Pas d'id pour session");
+          }
+          
           // La variable sessionRestaurée contient désormais l'objet précédent
           // qui avait été sauvegardé dans localStorage
           console.log(sessionRestaurée);
@@ -74,10 +107,28 @@
         
 
     })
+
     // control settings CrossValidationBlock
     .controller('CtrlSettingCrossBlock', function($scope, sharedId) {
         var id = sharedId.getProperty();
-        $scope.master = {Name: "CrossValidationBlock", ID: id, nameScript: "", interpreter: "", selection: "", inputs: [], outputs: [], parameters: []};
+        $scope.master = {Name: "CrossValidationBlock", ID: id, "CrossValidationType": ""};
+        
+        $scope.chooseTypeCV = function(type) {
+          // in function on the type we give the parameters
+          $scope.sequence["CrossValidationType"] = type ;
+          if(type == 'k-fold' || type == 'bootstrapping'){
+            $scope.sequence.parameters = { "CrossValidationCount" : "", 
+                                          "CrossValidationOutput" : [] };
+          }
+
+          console.log( typeof $scope.sequence.parameters["CrossValidationCount"]);
+        };
+
+        $scope.addCVOutput = function(temp1,value1) {
+          var temp = {temp1,value1} ;
+          $scope.sequence.parameters["CrossValidationOutput"].push(temp);
+        }
+
         $scope.reset = function() {
             $scope.sequence = angular.copy($scope.master);
         };
@@ -91,7 +142,14 @@
           // Ici, on reconvertit la chaîne en un objet 
           // JSON.stringify and saved in localStorage in JSON object again
           var sessionRestaurée = JSON.parse(localStorage.getItem(id));
-          $scope.sequence = sessionRestaurée;
+          try {
+            if (sessionRestaurée.ID != null)
+              $scope.sequence = sessionRestaurée;
+          } catch(e) {
+            // statements
+            console.log("Pas d'id pour session");
+          }
+          
           // La variable sessionRestaurée contient désormais l'objet précédent
           // qui avait été sauvegardé dans localStorage
           console.log(sessionRestaurée);
@@ -108,6 +166,7 @@
           var id = data.ID + '-' + post;
 
           data.nodes.push({name: newName, ID: id, nodes: []});
+          console.log(id);
       };
       $scope.saveDataId = function (id) {
         sharedId.setProperty(id);
